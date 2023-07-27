cdd052895e708281a41941aeeacbc4c029c1ac9f
Prakhar Agrawal <prakhar.agrawal31291@gmail.com>
2023-07-27 23:05:38 +0530

    Create Special Subarrays.java

---
+++ Interview problems/Sprinklr/Special Subarrays.java
@@ -0,0 +1,31 @@
+/*
+You are given an array Arr of size N consisting of non-negative integers. Find the number of subarrays of Arr, which are special.
+
+A subarray is called special if it satisfies the following condition:
+ 1. The product of max and min element of teh subarray is divisible by the length of teh subarray.
+
+Find the number of special subarrays of array Arr.
+
+Input Format:
+T - number of test cases
+Each test will have
+1. Size of array in line 1
+2. Elements of array in line 2
+
+Constraints:
+
+1<=T<=10^3
+1<=N<=5*10^4
+0<=Arr[i]<=30
+
+Example:
+2
+2
+1 5 
+3
+0 1 3
+
+Output
+2
+5
+*/
