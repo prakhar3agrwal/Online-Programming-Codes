f1ce990bcb183f7e230845351e1ad06052ee11a7
Prakhar Agrawal <prakhar.agrawal31291@gmail.com>
2024-11-16 23:12:27 +0530

    Update Contest1.java

--- Scaler/DSA/Intermediate/Contest1.java
+++ Scaler/DSA/Intermediate/Contest1.java
@@ -1,3 +1,5 @@
+//Nobel Integer
+
 public class Solution {
     public int solve(int[] A) {
         int ans = 0;
@@ -22,3 +24,39 @@ public class Solution {
 }
 
 //-6 -4 -2 -1 0
+
+
+//Divisibility by 7
+public class Solution {
+    public int[] solve(int[] A, int[][] B) {
+        int n = A.length;
+        int m = B.length;
+        int[] ans = new int[m];
+        
+        if(A[0]%7 == 0){
+            A[0]=1;
+        }else{
+            A[0]=0;
+        }
+        for(int i=1;i<n;i++){
+            if(A[i]%7==0){
+                A[i] = A[i-1]+1;
+            }
+            else{
+                A[i] = A[i-1];
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            int l = B[i][0];
+            int r = B[i][1];
+            int currAns = A[r];
+            if(l!=0){
+                currAns -= A[l-1];
+            }
+            
+            ans[i] = currAns;
+        }
+        return ans;
+    }
+}
